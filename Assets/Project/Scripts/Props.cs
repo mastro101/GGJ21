f835eb42d79fec8e1cd91f20444b76638f81512d
Matteo Mastropasqua <teo.mastropasqua@gmail.com>
2021-01-30 17:29:39 +0100

    scipt props

---
+++ Assets/Project/Scripts/Props.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Props : MonoBehaviour
+{
+    [SerializeField] float minForce = 0f;
+    [SerializeField] float maxForce = 1000f;
+
+    [SerializeField] UnityEvent OnCalciato;
+
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void CalcioSuperRandom()
+    {
+        rb.AddForce(VectorUtility.RandomV3(1f).normalized * Random.Range(minForce, maxForce));
+        OnCalciato?.Invoke();
+    }
+}
