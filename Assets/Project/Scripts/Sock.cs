2ab34d81e8d120bc8fc1565584bf57a361d5a3c7
Matteo Mastropasqua <teo.mastropasqua@gmail.com>
2021-01-30 15:03:08 +0100

    dotween + fuga della calza

--- Assets/Project/Scripts/Sock.cs
+++ Assets/Project/Scripts/Sock.cs
@@ -2,44 +2,87 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using DG.Tweening;
 
 [RequireComponent(typeof(NavMeshAgent))]
 [RequireComponent(typeof(ViewTriggerFromTransform))]
 public class Sock : MonoBehaviour
 {
     [SerializeField] float range;
+    [SerializeField] float velocitàDiFuga;
 
-    NavMeshAgent agent;
-    ViewTriggerFromTransform viewTrigger;
-    SockState state;
+    [SerializeField] float velocitàRotazioneInFugaComeLeGalline;
+    [SerializeField] float livelloDiFrappèNellaRotazione;
+
+    NavMeshAgent agenteNavigante;
+    ViewTriggerFromTransform vedoSeLoVedo;
+    SockState comeSto;
+
+    private void OnEnable()
+    {
+        vedoSeLoVedo.OnTriggerEnter += IDontLikeThereThisIsGoing;
+    }
 
     private void Awake()
     {
-        agent = GetComponent<NavMeshAgent>();
-        viewTrigger = GetComponent<ViewTriggerFromTransform>();
-        state = SockState.FuckingRun;
+        agenteNavigante = GetComponent<NavMeshAgent>();
+        vedoSeLoVedo = GetComponent<ViewTriggerFromTransform>();
+        comeSto = SockState.RandomMove;
     }
 
     private void Start()
     {
-        MoveToRandomPos();
+        MoveACazzoDiCane();
     }
 
     private void Update()
     {
-        if (state == SockState.FuckingRun)
+        if (comeSto == SockState.RandomMove)
         {
-            if (Vector3.Distance(agent.destination - (Vector3.up * agent.destination.y), transform.position - (Vector3.up * transform.position.y)) < 0.1f)
+            if (Vector3.Distance(agenteNavigante.destination - (Vector3.up * agenteNavigante.destination.y), transform.position - (Vector3.up * transform.position.y)) < 0.1f)
             {
-                MoveToRandomPos();
+                MoveACazzoDiCane();
             }
+        }
+        else if (comeSto == SockState.FuckingRun)
+        {
+            Run_TUUUUUTUTUTUTUTUTUTUTU(vedoSeLoVedo.GetObj());
+        }
 
+        //if (Input.GetKeyDown(KeyCode.Return))
+        //{
+        //    agenteNavigante.Move(Vector3.one);
+        //}
+    }
+
+    public void MoveACazzoDiCane()
+    {
+        agenteNavigante.SetDestination(VectorUtility.RandomV3OnPlaneY(range));
+    }
+
+    void ChangeState(SockState newState)
+    {
+        if (comeSto == SockState.RandomMove)
+        {
+            agenteNavigante.isStopped = true;
+        }
+        comeSto = newState;
+        if (comeSto == SockState.FuckingRun)
+        {
+            
         }
     }
 
-    public void MoveToRandomPos()
+    void IDontLikeThereThisIsGoing(Transform playerT)
     {
-        agent.SetDestination(VectorUtility.RandomV3OnPlaneY(range));
+        ChangeState(SockState.FuckingRun);
+    }
+
+    void Run_TUUUUUTUTUTUTUTUTUTUTU(Transform playerT)
+    {
+        Vector3 newPos = ((playerT.position - transform.position) * -1).normalized * velocitàDiFuga * Time.deltaTime;
+        agenteNavigante.Move(newPos);
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(newPos, transform.up), Time.deltaTime * livelloDiFrappèNellaRotazione);
     }
 }
 
