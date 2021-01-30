26d512e49417ff7d399b0853a00fd8ab471ae54d
Aron Gilli <arongilli@outlook.it>
2021-01-30 02:16:22 +0100

    altre cose di UI noiose

---
+++ Assets/Project/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    public bool hideTimer;
+    public bool muteSounds;
+    private void Awake() 
+    {
+        SetSingleton();
+    }
+    
+    void SetSingleton()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Project/Scripts/UITimer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UITimer : MonoBehaviour
+{
+    private TextMeshProUGUI timerText;
+    private void Start()
+    {
+        timerText = GetComponent<TextMeshProUGUI>();
+        Check();
+    }
+
+    void Check()
+    {
+        if (FindObjectOfType<UIManager>().hideTimer)
+        {
+            timerText.enabled = false;
+        }
+        else
+        {
+            timerText.enabled = true; 
+        }
+    }
+}
