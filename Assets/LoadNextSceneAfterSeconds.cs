3d288f84f445a759ea52c351076cbccdf72acbbe
Aron Gilli <arongilli@outlook.it>
2021-01-30 19:28:26 +0100

    intro e logo

---
+++ Assets/LoadNextSceneAfterSeconds.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextSceneAfterSeconds : MonoBehaviour
+{
+    public int seconds;
+    void Start()
+    {
+        Invoke("LoadScene", seconds);
+    }
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
