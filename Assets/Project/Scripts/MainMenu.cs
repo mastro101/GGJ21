f2aedc0063354e9f0acc389738876b14640658ab
Aron Gilli <arongilli@outlook.it>
2021-01-31 11:17:30 +0100

    main menu finito in teoria

--- Assets/Project/Scripts/MainMenu.cs
+++ Assets/Project/Scripts/MainMenu.cs
@@ -2,12 +2,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject startButton;
+
+    private void Start()
+    {
+        FindObjectOfType<EventSystem>().SetSelectedGameObject(startButton);
+    }
+
     public void StartNewGame()
     {
         LoadLevelsManager.instance.LoadGameScene();
+    }
+    
+    public void CloseGame()
+    {
+        Application.Quit();
     }
 }
